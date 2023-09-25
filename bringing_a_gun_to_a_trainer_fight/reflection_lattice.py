4f2947cea976e1a4c5fcbb8d5cf17477ca7f8750
Michael Haynes <antihaynes@gmail.com>
2023-09-24 19:35:16 -0500

    "Remove old versions of methods"

--- bringing_a_gun_to_a_trainer_fight/reflection_lattice.py
+++ bringing_a_gun_to_a_trainer_fight/reflection_lattice.py
@@ -5,7 +5,7 @@ Key insight: symmetry
 """
 
 from fractions import Fraction
-from math import sqrt, gcd, floor, ceil
+from math import sqrt, gcd, floor
 from itertools import chain
 
 
@@ -126,110 +126,6 @@ class ReflectionLattice:
             ymax -= ymax % (2 * self._dims.y)
             for y in range(ymin, ymax + 1, 2 * self._dims.y):
                 yield Point(x, y)
-
-    def numVisibleInDisk(self, center, radius):
-        z0 = center * self._inverse_basis
-        d1, d2 = z0.x.denominator, z0.y.denominator
-        lcm = d1 * d2 // gcd(d1, d2)
-
-        numVisible = self.numInDisk(center, radius)
-        numMasked = 0
-
-        sieve = [0] * (radius + 1)
-        for n in range(1, radius // lcm):
-            k = 1 + n * lcm
-            if sieve[k - 1] != 1:
-                redundant = self.numInDisk(center, radius // k)
-                multiplicity = 1 - sieve[k - 1]
-                correction = multiplicity * redundant
-                numVisible -= correction
-                numMasked += correction
-                for j in range(k - 1, len(sieve), k):
-                    sieve[j] += multiplicity
-        return numVisible, numMasked
-
-    def numInDisk(self, center, radius):
-        """
-        Computes the number of integer lattice points lying within the given radius of the point center.
-
-        Note that the "anchor point" of the lattice (i.e., its "origin") can be taken to be the same as center without loss of generality.
-        This is not true for the equivalent problem on visilibity classes, however.
-        """
-        # center of ellipse
-        ecenter = center * self._inverse_basis
-
-        # axes of ellipse
-        axes = radius * self._inverse_basis
-
-        mmin, mmax = ceil(ecenter.x - axes.x.x), floor(ecenter.x + axes.x.x)
-        print(f"Center of ellipse: {ecenter}\nAxes: {axes}")
-        print(
-            f"Checking x in range {2*self._dims.x*mmin + center.x, 2*self._dims.x*mmax + center.x}..."
-        )
-
-        total = 0
-        for m in range(mmin, mmax + 1):
-            s = (axes.y.y / axes.x.x) * sqrt(axes.x.x**2 - (m - ecenter.x)**2)
-            nmin, nmax = ceil(ecenter.y - s), floor(ecenter.y + s)
-            print(
-                f"Num points found with x = {2*self._dims.x*m + center.x}: {nmax - nmin + 1}",
-            )
-            print(f"\tyrange = {nmin, nmax}")
-            total += nmax - nmin + 1
-
-        return total
-
-    def _find_linear_combo(self, u, v):
-        """Finds the minimal k and modulus n such that u + k'*v is in self for all k' == k (mod n).
-
-        Raises a ValueError if this is not possible."""
-        try:
-            kx, nx = self.solveScalarCongruence(u.x, v.x, self._basis.x.x)
-            ky, ny = self.solveScalarCongruence(u.y, v.y, self._basis.y.y)
-            d, r, s = self.euclideanAlgorithm(nx, ny)
-
-            if kx % d != ky % d:
-                raise ValueError("2D congruence has no solution")
-
-            Q = (ky - kx) // d
-            assert Q * r * nx + kx == -Q * s * ny + \
-                ky, "Inconsistent results obtained for vector congruence"
-            lcm = (nx * ny // d)
-            return (Q * r * nx + kx) % lcm, lcm
-        except ValueError:
-            raise
-
-    @staticmethod
-    def solveScalarCongruence(x, y, z):
-        """Find the minimal k that solves z | (x + k*y), along with the modulus n such that k' == k (mod n) guarantees that x + k*y is a solution.
-
-        Raise a ValueError if this is not possible."""
-        d, a, b = ReflectionLattice.euclideanAlgorithm(y, z)
-        if x % d != 0:
-            raise ValueError("Congruence has no solution")
-        x //= d
-        y //= d
-        z //= d
-        yinv = a % z
-        return (-a * x) % z, z
-
-    @staticmethod
-    def euclideanAlgorithm(a, b):
-        """Computes r and s such that r*a + s*b = gcd(a, b), returning (gcd(a, b), r, s)"""
-        if a == b:
-            return a, 1, 0
-        if a > b:
-            d, r, s = ReflectionLattice.euclideanAlgorithm(b, a)
-            return d, s, r
-        ca, cb = [1, 0], [0, 1]
-        while a % b != 0:
-            d = a // b
-            a -= d * b
-            ca[0] -= d * cb[0]
-            ca[1] -= d * cb[1]
-            if a < b:
-                a, b, ca, cb = b, a, cb, ca
-        return b, cb[0], cb[1]
 
 
 if __name__ == "__main__":
